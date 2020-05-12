f109120e17163c10a2c643c930034b6bb7ccee6c
LloydFinch <wangwenqi@orangelab.cn>
2020-05-12 14:20:21 +0800

    add LeakCanary

--- app/src/main/java/com/devloper/lloydfinch/neteasedemo/MainApplication.java
+++ app/src/main/java/com/devloper/lloydfinch/neteasedemo/MainApplication.java
@@ -1,8 +1,14 @@
 package com.devloper.lloydfinch.neteasedemo;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 public class MainApplication extends Application {
+
+    private static final String TAG = "MainApplication";
 
     private static MainApplication instance;
 
@@ -14,5 +20,15 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+
+        /**
+         * 初始化内存泄漏检测器
+         */
+        {
+            if (!LeakCanary.isInAnalyzerProcess(this)) {
+                RefWatcher refWatcher = LeakCanary.install(this);
+                Log.d(TAG, "onCreate: refWatcher = " + refWatcher);
+            }
+        }
     }
 }
